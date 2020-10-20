332cb5a73b6e54af46a31a5de6abbc04788dd441
D-NET2 <61761709+D-NET2@users.noreply.github.com>
2020-10-20 20:03:07 +0100

    Update Greeter.java

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,5 @@
 public class Greeter{
     public static void main(String args[]){
-        System.out.println("Hello world!\n(And all the people of the world)");
+        System.out.println("Hello world!\n(And all the people of the world)");//prints this sentence on CLI
     }
 }
