078fdc67ea55080fb41c3fa2db1c1bd82dbd1d25
Dahir <61761709+D-NET2@users.noreply.github.com>
2020-10-26 20:34:24 +0000

    Add files via upload

---
+++ week3-046.AverageOfGivenNumbers/src/AverageOfGivenNumbers.java
@@ -0,0 +1,20 @@
+
+public class AverageOfGivenNumbers {
+
+    public static double average(int number1, int number2, int number3, int number4) {
+        int sum = sum(number1, number2, number3, number4);
+        double average = (double)sum/4 ;
+        return average;
+        // write your code here
+        
+    }
+
+    public static void main(String[] args) {
+        double answer = average(4, 3, 6, 1);
+        System.out.println("average: " + answer);
+    }
+    
+    public static int sum(int number1, int number2, int number3, int number4) {
+    return number1 + number2 + number3 +number4;
+    }
+}
